67aa383d451e9b0a2a52e5c1bd953647082cfd99
Robert Sinclair <0wwafa@gmail.com>
2024-05-27 21:47:50 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from shell import __version__
+
+setup(
+    name='shell',
+    version=__version__,
+
+    url='https://github.com/0wwafa/shelle',
+    author='Robert Sinclair',
+    author_email='0wwafa@gmail.com',
+
+    py_modules=['shell'],
+)
