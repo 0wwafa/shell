1296fd731e741759b0208cd90186b872ed4a152a
Robert Sinclair <0wwafa@gmail.com>
2024-06-03 00:27:35 +0300

    Update shell.py

--- shell.py
+++ shell.py
@@ -7,7 +7,7 @@ import string
 
 __version__ = 'dev'
 
-def shell(colab=False):
+def shell(colab=False,pw=''):
 	
     print("Starting up...")
 
@@ -29,8 +29,9 @@ def shell(colab=False):
 
     with open('log', 'r') as f:
         hh = [line for line in f if "tunneled" in line][-1].split()[5]
-
-    pw = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
+    if pw=='':
+        pw = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
+        print(f"\nYour password is: {pw}\n")
 
     with open('dosh', 'w') as f:
         f.write('''#!/bin/bash
@@ -46,7 +47,7 @@ def shell(colab=False):
 
     print("Starting server...")
     print(f"Your server is: {hh}")
-    print(f"\nYour password is: {pw}\n")
+
     subprocess.Popen(["./ttyd", "-p", "8568", "--writable", "-t", "fontSize=20", "-t", "theme={'foreground':'#d2d2d2','background':'#1b1b1b','cursor':'#adadad','black':'#000000','red':'#d81e00','green':'#5ea702','yellow':'#cfae00','blue':'#427ab3','magenta':'#89658e','cyan':'#00a7aa','white':'#dbded8','brightBlack':'#686a66','brightRed':'#f54235','brightGreen':'#99e343','brightYellow':'#fdeb61','brightBlue':'#84b0d8','brightMagenta':'#bc94b7','brightCyan':'#37e6e8','brightWhite':'#f1f1f0'}", "-t", "fontFamily='Menlo For Powerline,Consolas,Liberation Mono,Menlo,Courier,monospace'", "-t", "enableTrzsz=true", "./dosh"], stdout=open(os.devnull, 'w'), stderr=subprocess.STDOUT)
     if(colab):
         from IPython.display import IFrame
