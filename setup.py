2b91102bf5ad6413f6161a1accb40dae90b8636f
Robert Sinclair <0wwafa@gmail.com>
2024-06-21 06:49:36 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='shell',
-    version='0.1.0',    
+    version='0.1.1',    
     description='A shell, anywhere',
     url='https://github.com/0wwafa/shell',
     author='Robert Sinclair',
